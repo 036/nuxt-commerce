f88ef46e1cda5080d88dd80f1a3d0f56df39cc04
Rob <rob@nulldev.uk>
2019-10-07 21:35:35 +0100

    Add scope for parent categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public function scopeParents(Builder $builder)
+    {
+        $builder->whereNull('parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,9 @@
 <?php
 
-//
+use App\Models\Category;
+
+Route::get('/', function () {
+    $categories = Category::get();
+
+    dd($categories);
+})
